e7633bc0fc4d2a783da8e2e42f6290920f80c082
Ankit-tiwarii01 <ankittiwari17417@gmail.com>
2026-04-04 13:58:09 +0530

    removed npm packages

--- Prompt2UI/src/App.jsx
+++ Prompt2UI/src/App.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
 import"./App.css"
+import Home  from './pages/home';
 import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
     <BrowserRouter>
-      {/* Routes */}
-      <Routes>
+    <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<nopage />} />
       </Routes>
     </BrowserRouter>
-    </>
   )
 }
 
--- Prompt2UI/src/pages/home.jsx
+++ Prompt2UI/src/pages/home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 
-const home = () => {
+const Home = () => {
   return (
     <>
-    <Navbar/>
-    home
+    <p>This is para</p>
     </>
   )
 }
 
-export default home
+export default Home
