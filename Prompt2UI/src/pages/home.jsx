4c5b34e4dc5ebdef0ea3658c771aedee8a772c26
Ankit-tiwarii01 <ankittiwari17417@gmail.com>
2026-04-05 17:43:11 +0530

    Main UI Design

--- Prompt2UI/src/pages/home.jsx
+++ Prompt2UI/src/pages/home.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 
-const home = () => {
+const Home = () => {
   return (
     <>
-    <Navbar/>
-    home
+      <Navbar />
+
+      <div className="flex items-center justify-between px-[100px] gap-[30px]">
+        
+        <div className="w-[50%] h-[80vh] bg-[#141319] mt-5 p-[20px]">
+          <h3 className="text-[25px] Font-semibold sp-text">
+            AI Component Generator
+          </h3>
+          <p className="text-[gray] mt-2 text-[16px]">Please Describe your Component  </p>
+        </div>
+
+        <div className="w-[50%] h-[80vh] bg-[#141319] mt-5"></div>
+
+      </div>
     </>
   )
 }
 
-export default home
+export default Home
